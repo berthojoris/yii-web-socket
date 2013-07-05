9cf6cf2c2e119086331be705cad166ba75ca5447
Once <unnfly@gmail.com>
2013-07-05 16:29:55 +0300

    add some methods to event handler abstract class

--- src/event/Handler.php
+++ src/event/Handler.php
@@ -18,12 +18,36 @@ class Handler extends \YiiWebSocket\Component implements \YiiWebSocket\IClientEm
 		return \YiiWebSocket\Socket::current();
 	}
 
+	/**
+	 * @param string|int $path
+	 *
+	 * @return \YiiWebSocket\Extension\Path
+	 */
+	final public function path($path) {
+		return \YiiWebSocket\Extension\Path::get($path);
+	}
+
+	/**
+	 * @param string|int $room
+	 *
+	 * @return \YiiWebSocket\Extension\Room
+	 */
+	final public function room($room) {
+		return \YiiWebSocket\Extension\Room::getRoom($room);
+	}
+
 	final public function emit() {
-		return call_user_func_array(array($this->getSocket(), 'emit'), func_get_args());
+		$socket = $this->getSocket();
+		if ($socket) {
+			call_user_func_array(array($socket, 'emit'), func_get_args());
+		}
 	}
 
 	final public function broadcast() {
-		return call_user_func_array(array($this->getSocket(), 'broadcast'), func_get_args());
+		$socket = $this->getSocket();
+		if ($socket) {
+			call_user_func_array(array($socket, 'broadcast'), func_get_args());
+		}
 	}
 
 	public function onConnection() {
